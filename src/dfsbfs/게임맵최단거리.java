95abc1f958a80ce684ac19d4c1d1adf2ae317dd4
조건호 <soo_1992@naver.com>
2022-11-03 11:49:35 +0900

    Update 게임맵최단거리.java

--- src/dfsbfs/게임맵최단거리.java
+++ src/dfsbfs/게임맵최단거리.java
@@ -2,35 +2,47 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
-public int solution(int[][] maps) {
+    public int solution(int[][] maps) {
         int[] directionsX = {-1, 1, 0, 0};
         int[] directionsY = {0, 0, -1, 1};
+        int n = maps.length;
+        int m = maps[0].length;
 
         maps[0][0] = 2;
-        Queue<Integer[]> q = new LinkedList<>();
-        q.add(new Integer[]{0, 0});
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0, 0));
 
 
         while (!q.isEmpty()) {
-            Integer[] currentPosition = q.poll();
-            int x = currentPosition[0];
-            int y = currentPosition[1];
+            Node currentPosition = q.poll();
+            int currentX = currentPosition.x;
+            int currentY = currentPosition.y;
 
             for (int i = 0; i < 4; i++) {
-                int newX = x + directionsX[i];
-                int newY = y + directionsY[i];
+                int nextX = currentX + directionsX[i];
+                int nextY = currentY + directionsY[i];
 
-                if (newX >= 0 && newX < maps.length && newY >= 0 && newY < maps[0].length && maps[newX][newY] == 1) {
-                    maps[newX][newY] = maps[x][y] + 1;
-                    q.add(new Integer[]{newX, newY});
+                if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < m && maps[nextX][nextY] == 1) {
+                    maps[nextX][nextY] = maps[currentX][currentY] + 1;
+                    q.add(new Node(nextX, nextY));
                 }
             }
         }
 
-        if (maps[maps.length-1][maps[0].length-1] == 1) {
+        if (maps[n - 1][m - 1] == 1) {
             return -1;
         } else {
-            return maps[maps.length-1][maps[0].length-1]-1;
+            return maps[n - 1][m - 1] - 1;
+        }
+    }
+
+    class Node {
+        int x;
+        int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 }
