d1edd2b952b79bda0edb67825c7e3499fe2bd482
조건호 <soo_1992@naver.com>
2022-11-10 13:50:39 +0900

    Create 모의고사.java

---
+++ src/completeSearch/모의고사.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        List<Integer> answerList = new ArrayList<>();
+        int[][] supoja = {
+                {1, 2, 3, 4, 5},
+                {2, 1, 2, 3, 2, 4, 2, 5},
+                {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+        };
+
+        Map<Integer, Integer> scoreMap = new HashMap<>();
+        int[] scoreArray = new int[]{0, 0, 0};
+        for (int i = 0; i < supoja.length; i++) {
+            for (int j = 0; j < answers.length; j++) {
+                if (supoja[i][j % (supoja[i].length)] == answers[j]) {
+                    scoreArray[i] += 1;
+                }
+            }
+        }
+
+        int maxScore = -1;
+        for (int score : scoreArray) {
+            if (score > maxScore) {
+                maxScore = score;
+            }
+        }
+
+        for (int i = 0; i < scoreArray.length; i++) {
+            if (scoreArray[i] == maxScore) {
+                answerList.add(i + 1);
+            }
+        }
+
+
+        return answerList.stream()
+                .mapToInt(i -> i)
+                .toArray();
+    }
+}
